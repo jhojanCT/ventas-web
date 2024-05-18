fbc84cfab5ada18fd2bf3b89de16639d214ab009
jhojanCT <jhojancoro1@gmail.com>
2024-05-17 20:51:25 -0400

    Create: UserSeeder, Moder User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call(UserSeeder::class);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+           // 'name' => 'Test User',
+           // 'email' => 'test@example.com',
+//
+           // 'name' => 'Bruno',
+           // 'email' => 'Jhojancoro1@gmail.com',
 
-            'name' => 'Bruno',
-            'email' => 'Jhojancoro1@gmail.com',
+           
     
         ]);
     }
