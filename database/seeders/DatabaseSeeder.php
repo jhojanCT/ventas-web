6d45b19bc31c6618cf1e1b77cf4ff64d0eebebc3
jhojanCT <jhojancoro1@gmail.com>
2024-05-24 14:26:40 -0400

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,15 +34,15 @@ class DatabaseSeeder extends Seeder
      //       DetailSaleSeeder::class,
      //   ]);
 
-        User::factory(10)->create();
-        Role::factory(10)->create();
-        Category::factory(10)->create();
-        Person::factory(10)->create();
-        Article::factory(10)->create();
-        Entry::factory(10)->create();
-        DetailEntry::factory(10)->create();
-        Sale::factory(10)->create();
-        DetailSale::factory(10)->create();
+       // User::factory(10)->create();
+       // Role::factory(10)->create();
+       // Category::factory(10)->create();
+       // Person::factory(10)->create();
+       // Article::factory(10)->create();
+       // Entry::factory(10)->create();
+       // DetailEntry::factory(10)->create();
+       // Sale::factory(10)->create();
+       // DetailSale::factory(10)->create();
         
         // User::factory(10)->create();
         //$this->call(UserSeeder::class);
